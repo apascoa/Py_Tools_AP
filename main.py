1eb8480a44177dafaf262c5d126a951554e81b76
Abílio <abiliodpascoa@gmail.com>
2022-01-14 16:56:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@
 #test
 def test():
     print("Working")
+
+#Square_title
+def square_title(text):
+    largura = len(text)+4
+    print("┏",(largura-2)*"-","┓\n| ",text," |\n┗",(largura-2)*"-","┛", sep="")
+
+#Underline
+def underline(text):
+    print(len(text)*"-")
